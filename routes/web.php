bc6ddaa3772252a220f544cd260705945edeb55e
saikyo muteki <db4649t2006rsk@gmail.com>
2022-12-03 10:58:19 +0900

    第5段階 カート追加機能

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,8 @@ Route::name('product.')
     Route::get('/', 'ProductController@index')->name('index');
     Route::get('/product/{id}', 'ProductController@show')->name('show');
   });
+
+Route::name('line_item.')
+  ->group(function () {
+    Route::post('/line_item/create', 'LineItemController@create')->name('create');
+  });
