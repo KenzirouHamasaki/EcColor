757ad4ad65d39ec6569346a24a68652c9210ca0d
saikyo muteki <db4649t2006rsk@gmail.com>
2022-12-03 11:16:50 +0900

    第6段階 カート画面

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-light bg-light">
   <div class="container">
       <a class="navbar-brand" href="/">{{ config('app.name', 'Laravel') }}</a>
-      <a class="fa-brands fa-waze" href="#"></a>
+      <a class="fa-brands fa-waze" href="{{ route('cart.index') }}"></a>
   </div>
 </nav>
