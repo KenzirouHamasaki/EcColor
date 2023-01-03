2cb0b56491ff1528f688db8deeda19854e97eaa6
saikyo muteki <db4649t2006rsk@gmail.com>
2023-01-03 23:57:09 +0900

    第12段階 全ページ背景

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -5,30 +5,32 @@
 @endsection
 
 @section('content')
-<div class="container">
-    <div class="product">
-        <img src="{{ asset($product->image) }}" class="product-img"/>
-        <div class="product__content-header text-center">
-            <div class="product__name">
-                {{ $product->name }}
+<div class="back-ground">
+    <div class="container">
+        <div class="product">
+            <img src="{{ asset($product->image) }}" class="product-img"/>
+            <div class="product__content-header text-center">
+                <div class="product__name">
+                    {{ $product->name }}
+                </div>
+                <div class="product__price">
+                    ¥{{ number_format($product->price) }}
+                </div>
             </div>
-            <div class="product__price">
-                ¥{{ number_format($product->price) }}
-            </div>
+            {{ $product->explanatory }}
+            <form method="POST" action="{{ route('line_item.create') }}">
+                @csrf
+                <input type="hidden" name="id" value="{{ $product->id }}"/>
+                <div class="product__quantity">
+                    <input type="number" name="quantity" min="1" value="1" require/>
+                </div>
+                <div class="product__btn-add-cart">
+                    <button type="submit" class="btn btn-outline-secondary">カートに追加する</button>
+                </div>
+            </form>
         </div>
-        {{ $product->explanatory }}
-        <form method="POST" action="{{ route('line_item.create') }}">
-            @csrf
-            <input type="hidden" name="id" value="{{ $product->id }}"/>
-            <div class="product__quantity">
-                <input type="number" name="quantity" min="1" value="1" require/>
-            </div>
-            <div class="product__btn-add-cart">
-                <button type="submit" class="btn btn-outline-secondary">カートに追加する</button>
-            </div>
-        </form>
+        <a href="{{ route('product.index') }}">TOPへ戻る<a>
     </div>
-    <a href="{{ route('product.index') }}">TOPへ戻る<a>
 </div>
 @endsection
 
